d4e2b058302622f2aa6699f9f613b7595bf87106
Sofya Dziamidava <sofya.dziamidava@gmail.com>
2022-03-30 13:06:44 +0200

    fixed description

--- src/RewriteMe.java
+++ src/RewriteMe.java
@@ -8,12 +8,11 @@ Inlämningsuppgit i kursen Funktionell Programmering för JAVA-programmet
 För samtliga funktioner i denna fil, byt ut "throw UnSupportedException"-raden
 och skriv ett pipeline-uttryck som returnerar det som ska returneras.
 Streams MÅSTE användas i samtliga funktioner som lämnas in
-För att testa om era funktioner funkar, kör testerna som hör till denna fil
-För att bli godkänd på denna uppgift måste minst 7 av funktionerna vara implementerade.
+För att testa om era funktioner funkar, kör testerna som hör till denna fil.
 Sigruns bedömning av denna uppgift kommer att gå till så att hon klipper in er version av denna fil
 i sitt projekt och kör testerna.
 Hennes kontroll om ni har klarat uppgifterna eller inte görs genom att
-hon kollar att tillräckeligt många av tester går gröna. Pga detta ska ni inte ändra i någon fil
+hon kollar att tester går gröna. Pga detta ska ni inte ändra i någon fil
 medföljande detta projekt, och inte heller metodsignaturerna i denna fil eller era tester, eftersom
 ni då riskerar att påverka rättningen i negativ riktning.
  */
@@ -66,18 +65,6 @@ public class RewriteMe {
         List<String> onlyAnswers = questions.stream().map(Question::getAllAnswers).flatMap(value -> value.stream())
                 .collect(Collectors.toList());
         return (int) onlyAnswers.stream().filter(s -> s.equals(answerCandidate)).count();
-    }
-
-    //Skapa en Map där kategorierna är nycklar och värdena är en lista
-    //av de frågesträngar som tillhör varje kategori
-    public Map<Category, List<String>> getQuestionGroupedByCategory() {
-        return questions.stream().collect(Collectors.toMap(l -> l.getCategory(), n -> getAllQuestionStringsBelongingACategory(n.getCategory())));
-    }
-
-    //Skapa en funktion som hittar det svarsalternativ som har flest bokstäver, i en kategori, given som inparameter
-    // OBS: Du måste använda Reduce!
-    public String getLongestLettercountAnwerInAGivenCategory(Category c) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 
